951c67f12b351d6ce7ab698ee2a03b2b724d000c
abdellah khouden <khoudenak@gmail.com>
2024-12-06 12:17:19 +0100

    Add relationships between Categorie and Produit models

--- app/Models/Categorie.php
+++ app/Models/Categorie.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Categorie extends Model
 {   
     use HasFactory;
     protected $fillable = ['nom', 'description'];
+
+    public function produits():HasMany{
+        return $this->hasMany(Produit::class);
+    }
 }
--- app/Models/Produit.php
+++ app/Models/Produit.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Produit extends Model
 {   
     use HasFactory;
     protected $fillable = ['nom', 'prix', 'quantite', 'description', 'categorie_id'];
+
+   public function categorie():BelongsTo{
+        return $this->belongsTo(Categorie::class);
+    }
 }
